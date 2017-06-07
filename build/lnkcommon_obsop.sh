2d45670737955516888f899e8834d450f2bb07e0
Jili Dong <Jili.Dong@noaa.gov>
2017-06-07 18:11:53 +0000

    update build/make and build/link for HYCOM

--- build/lnkcommon_obsop.sh
+++ build/lnkcommon_obsop.sh
@@ -32,12 +32,25 @@ cp $LETKFDIR/letkf_local.f90 ./
 cp $LETKFDIR/letkf_tools.f90 ./
 cp $LETKFDIR/vars_obs.f90 ./
 
+if [ ${model} == "hycom" ]; then
+   cp $MODELDIR/mod_xc.F ./
+   cp $MODELDIR/mod_za.F ./
+   cp $MODELDIR/mod_ppsw.F ./
+   cp $MODELDIR/wtime.F ./
+   cp $MODELDIR/hycom_intrp.f ./
+   cp $MODELDIR/layer2z.f ./
+fi
+
+
 OBSDIR=$root/src/obs
 cp $OBSDIR/params_obs.f90 ./
 cp $OBSDIR/compute_profile_error.f90 ./
 cp $OBSDIR/read_argo.f90 ./
 cp $OBSDIR/read_avhrr_pathfinder.f90 ./
 cp $OBSDIR/read_aviso_adt.f90 ./
+cp $OBSDIR/read_bufr.f90 ./
+cp $OBSDIR/obsop_bufr_tprof.f90 ./
+cp $OBSDIR/obsop_bufr_sprof.f90 ./
 cp $OBSDIR/obsop_tprof.f90 ./
 cp $OBSDIR/obsop_sprof.f90 ./
 cp $OBSDIR/obsop_sst.f90 ./
@@ -45,6 +58,13 @@ cp $OBSDIR/obsop_adt.f90 ./
 cp $OBSDIR/gsw_pot_to_insitu.f90 ./
 cp $OBSDIR/read_ice_txt.f90 ./
 cp $OBSDIR/obsop_icefrac.f90 ./
+
+if [ ${model} == "hycom" ]; then
+   cp $OBSDIR/read_bufr_hycom.f90 ./
+   cp $OBSDIR/obsop_bufr_tprof_hycom.f90 ./
+   cp $OBSDIR/obsop_bufr_sprof_hycom.f90 ./
+fi
+
 
 # Software package for equation of state computation from TEOS-2010
 GSWDIR=$root/src/obs/gsw_fortran_v3_03
